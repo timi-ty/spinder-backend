736329712b7eafaaafe92d4f898acc6e789d7eee
Timi Tayo <timilehin.ty@gmail.com>
2024-04-29 22:03:19 +0100

    Added 3 new radio sources

--- src/discover/discover.radio.ts
+++ src/discover/discover.radio.ts
@@ -4,9 +4,41 @@ const afrobeatsApril2024: DiscoverSource = {
   //https://open.spotify.com/playlist/17tLe3tCu3NGUisFViFRSi?si=4316485a78874f74
   type: "Radio",
   id: "17tLe3tCu3NGUisFViFRSi",
-  name: "Spindr Radio",
+  name: "Spindr Afro Radio",
   image:
     "https://img.etimg.com/thumb/msid-89919334,width-300,height-225,imgsize-35954,resizemode-75/radio.jpg", //Explore storing these app level images in firebase storage instead of at the frontend.
 };
 
-export const spindrRadio = [afrobeatsApril2024];
+const alteCruise: DiscoverSource = {
+  //https://open.spotify.com/playlist/37i9dQZF1DX5ja5oV6Kto0?si=bd37de31f7ab4e2d
+  type: "Radio",
+  id: "37i9dQZF1DX5ja5oV6Kto0",
+  name: "Spindr Alte Radio",
+  image:
+    "https://img.etimg.com/thumb/msid-89919334,width-300,height-225,imgsize-35954,resizemode-75/radio.jpg", //Explore storing these app level images in firebase storage instead of at the frontend.
+};
+
+const hipHopMix: DiscoverSource = {
+  //https://open.spotify.com/playlist/37i9dQZF1EQnqst5TRi17F?si=9a0ea31575f940f6
+  type: "Radio",
+  id: "37i9dQZF1EQnqst5TRi17F",
+  name: "Spindr Hip Hop Radio",
+  image:
+    "https://img.etimg.com/thumb/msid-89919334,width-300,height-225,imgsize-35954,resizemode-75/radio.jpg", //Explore storing these app level images in firebase storage instead of at the frontend.
+};
+
+const indieMix: DiscoverSource = {
+  //https://open.spotify.com/playlist/37i9dQZF1EQqkOPvHGajmW?si=70779828c59543c8
+  type: "Radio",
+  id: "37i9dQZF1EQqkOPvHGajmW",
+  name: "Spindr Afro Radio",
+  image:
+    "https://img.etimg.com/thumb/msid-89919334,width-300,height-225,imgsize-35954,resizemode-75/radio.jpg", //Explore storing these app level images in firebase storage instead of at the frontend.
+};
+
+export const spindrRadio = [
+  afrobeatsApril2024,
+  alteCruise,
+  hipHopMix,
+  indieMix,
+];
